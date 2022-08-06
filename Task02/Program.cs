c9d2948c951870d9a0d8443e9cdc223470d0aa28
Dmitry <Logan6338@mail.ru>
2022-08-06 21:07:16 +0300

    Задача №4+

---
+++ Task02/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Напишите первое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Напишите второе число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Напишите третье число");
+int numberD = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+
+if ((numberA > numberB) && (numberA > numberD))
+{
+    Console.WriteLine(numberA);
+}
+else
+if ((numberB > numberA) && (numberB > numberD))
+{
+    Console.WriteLine(numberB);
+}
+else
+if ((numberD > numberA) && (numberD > numberB))
+{
+    Console.WriteLine(numberD);
+}
