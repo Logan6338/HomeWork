c691b3ac43e463a32a261594ed28ba6ec337d30d
Dmitry <Logan6338@mail.ru>
2022-08-06 21:19:24 +0300

    Задача №6

---
+++ Task03/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Напишите число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+if ((numberA % 2) == 0)
+{
+    Console.WriteLine("число четное");
+}
+else
+{
+    Console.WriteLine("число нечетное");
+}
