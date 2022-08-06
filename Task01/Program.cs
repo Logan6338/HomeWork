dc795b2a71b92663c72151e50d4ec0d496408e0b
Dmitry <Logan6338@mail.ru>
2022-08-06 20:08:47 +0300

    Задача №2

---
+++ Task01/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Напишите первое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Напишите второе число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA > numberB)
+{
+    Console.WriteLine("Первое число больше второго");
+}
+else
+if (numberA < numberB)
+{
+    Console.WriteLine("Первое число меньше второго");
+}
+else
+{
+    Console.WriteLine("Обы числа равны");
+}
