cb3ecc9724bc39c9c49f6d61e2eeebc2f5a2c1e8
Dmitry <Logan6338@mail.ru>
2022-08-07 18:40:26 +0300

    Задача № 8

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -1,11 +1,8 @@
 ﻿Console.WriteLine("Напишите число");
 int N = Convert.ToInt32(Console.ReadLine());
-   for(int i=2; i<=N; i++)
-   {
-   if(i%2 == 0)   
+   for(int i=2; i<=N; i+=2)
    {
     Console.Write("{0} ", i);
-   }
    }
   Console.ReadKey();
   return 0;
